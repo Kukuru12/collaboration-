b33b7a338c48b3e04ce7d035952b82d511339718
int wildan <int-wildan@lalampa.me>
2023-02-15 11:04:29 +0700

    baru

--- kondisi1.js
+++ kondisi1.js
@@ -1,12 +1,14 @@
-var jmlAngkot = 10
-var angkotBeroperasi = 6
+var jmlAngkot = 2;
+var angkotBeroperasi = 6;
 
-for( var noAngkot = 1; noAngkot <=jmlAngkot; noAngkot++) {
-   if(noAngkot <= 6 ) {
-      console.log('Angkot no. ' + noAngkot + ' Beroperasi dengan baik')
-   } else if(noAngkot === 8 || noAngkot === 10) {
-      console.log('Angkot no. ' + noAngkot + ' Sedang lembur')
-   } else {
-      console.log('Angkot no. ' + noAngkot + ' Sedang tidak beroperasi')
-   }
+for (var noAngkot = 1; noAngkot <= jmlAngkot; noAngkot++) {
+  if (noAngkot <= 6) {
+    console.log("Angkot no. " + noAngkot + " Beroperasi dengan baik");
+  } else if (noAngkot === 8 || noAngkot === 10) {
+    console.log("Angkot no. " + noAngkot + " Sedang lembur");
+  } else {
+    console.log("Angkot no. " + noAngkot + " Sedang tidur");
+  }
 }
+
+console.log("fitur baru");
