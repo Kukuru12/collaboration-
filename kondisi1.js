9d18876e083f402770b36a396e4cae818416eb88
int adzan <int-adzan@lalampa.me>
2023-02-15 13:56:09 +0700

    mengembalikan perintah ke semula

--- kondisi1.js
+++ kondisi1.js
@@ -1,14 +1,12 @@
-var jmlAngkot = 2;
-var angkotBeroperasi = 6;
+var jmlAngkot = 10
+var angkotBeroperasi = 6
 
-for (var noAngkot = 1; noAngkot <= jmlAngkot; noAngkot++) {
-  if (noAngkot <= 6) {
-    console.log("Angkot no. " + noAngkot + " Beroperasi dengan baik");
-  } else if (noAngkot === 8 || noAngkot === 10) {
-    console.log("Angkot no. " + noAngkot + " Sedang lembur");
-  } else {
-    console.log("Angkot no. " + noAngkot + " Sedang tidur");
-  }
+for( var noAngkot = 1; noAngkot <=jmlAngkot; noAngkot++) {
+   if(noAngkot <= 6 ) {
+      console.log('Angkot no. ' + noAngkot + ' Beroperasi dengan baik')
+   } else if( noAngkot === 8 || noAngkot === 10) {
+      console.log('Angkot no. ' + noAngkot + ' Sedang lembur')
+   } else {
+      console.log('Angkot no. ' + noAngkot + ' Sedang tidak beroperasi')
+   }
 }
-
-console.log("fitur baru");
