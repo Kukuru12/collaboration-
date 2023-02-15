55cd1b662f7d7c499ebfff5b621753a9a8d1ff94
int adzan <int-adzan@lalampa.me>
2023-02-15 10:37:53 +0700

    menambahkan log js

---
+++ kondisi1.js
@@ -0,0 +1,12 @@
+var jmlAngkot = 10
+var angkotBeroperasi = 6
+
+for( var noAngkot = 1; noAngkot <=jmlAngkot; noAngkot++) {
+   if(noAngkot <= 6 ) {
+      console.log('Angkot no. ' + noAngkot + ' Beroperasi dengan baik')
+   } else if(noAngkot === 8 || noAngkot === 10) {
+      console.log('Angkot no. ' + noAngkot + ' Sedang lembur')
+   } else {
+      console.log('Angkot no. ' + noAngkot + ' Sedang tidak beroperasi')
+   }
+}
